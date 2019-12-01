6be2bb0afbaab44791c3b5ae118a480a2034151b
Christian Wang <christian.gnaw@gmail.com>
2019-12-01 14:40:26 -0500

    Update layout.jsx

--- src/app/views/layout.jsx
+++ src/app/views/layout.jsx
@@ -4,7 +4,7 @@ export default ((props) => (
     <html>
         <head>
             <meta name="viewport" content="width=device-width, inital-scale=1.0"/>
-            <title>{`${(props.title) ? props.title + " | " : ""}CPS530-Term-Project`}</title>
+            <title>{`${(props.title) ? props.title + " | " : ""}Ronin-React-View`}</title>
         </head>
         <body>
             {props.children}
